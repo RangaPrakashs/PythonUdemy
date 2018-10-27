0d320e180f3854da22cdf94f21122fe6037831ab
Ranga Prakash S <RangaPrakashs@users.noreply.github.com>
2018-10-27 18:33:55 +0530

    added first Excersise for Rock paper scissors
    This is my additional add apart from what the course teaches. 
    for each logic, I'll write a exercise to further my understandings.

---
+++ rockpaperscissors.py
@@ -0,0 +1,39 @@
+import random
+
+computerChoice = random.randint(1 , 3)
+
+print("1 : Rock" + "\n" + "2 : Paper" + "\n" + "3 : Scissors")
+
+player_one = input("Enter your choice player one\n ")
+player_two = computerChoice
+
+
+rock = 1
+paper = 2
+scissors = 3
+wins = "None"
+choices= {1: "rock", 2: "paper", 3: "scissors"}
+draw = False
+
+if player_one and player_two:
+    player_one = int(player_one)
+    player_two = int(player_two)
+    if player_one > 0 and player_two <= 3:
+        if player_one == rock and player_two == scissors:
+            print("Player One Wins as Rock beats scissors")
+        elif player_one == paper and player_two == rock:
+            print("Player One Wins as Paper beats Rock")
+        elif player_one == scissors and player_two == paper:
+            print("Player one Wins as scissors beats paper ")
+        elif player_one == rock and player_two == rock:
+            print("It's a draw")
+        elif player_one == paper and player_two == paper:
+            print("It's a Draw!")
+        elif player_one == scissors and player_two == scissors:
+            print("It's a draw!")
+        else:
+                print(f"Computer wins as {choices[player_two]} beats {choices[player_one]}")
+    else:
+        "Not valid in the menu"
+else:
+    print("That's not a valid choice")
